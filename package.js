feb49120bf829f05e49c0c506add0de09d4f69e9
Cristian <80855535+Khrusty1@users.noreply.github.com>
2024-01-19 04:29:53 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "smart-wallet-script",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "dev": "ts-node index.ts"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@thirdweb-dev/chains": "^0",
+    "@thirdweb-dev/sdk": "^3",
+    "@thirdweb-dev/wallets": "^1",
+    "dotenv": "^16.0.3",
+    "ethers": "^5"
+  },
+  "devDependencies": {
+    "@types/node": "^18.11.17",
+    "ts-node": "^10.9.1",
+    "typescript": "^4.9.4"
+  }
+}
